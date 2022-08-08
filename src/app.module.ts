a90cd6a67f36faeb578952f86c80524173fa8dbd
Saleh Marouf <stm1998@hotmail.com>
2022-08-08 12:20:45 +0300

    inject all modules in app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,14 @@ import { Module } from '@nestjs/common';
 
 import { UserModule } from './modules/user/user.module';
 import { DatabaseModule } from './modules/database/database.module';
+import { ProfileModule } from './modules/profile/profile.module';
+import { SkillsModule } from './modules/skills/skills.module';
 
 import config from 'config';
+import { ExperiencesModule } from './modules/experiences/experiances.module';
+import { EducationsModule } from './modules/educations/educations.module';
+import { CertificationsModule } from './modules/certifications/certifications.module';
+import { ActivitiesModule } from './modules/activities/activities.module';
 
 @Module({
   imports: [
@@ -15,6 +21,12 @@ import config from 'config';
       load: [config],
       isGlobal: true,
     }),
+    ProfileModule,
+    SkillsModule,
+    ExperiencesModule,
+    EducationsModule,
+    CertificationsModule,
+    ActivitiesModule,
   ],
 })
 export class AppModule {}

7bec0c76b6377edbdf645ba5f76f039162ad076f
Saleh Marouf <stm1998@hotmail.com>
2022-08-08 12:18:48 +0300

    add models to database providers

--- src/modules/database/database.provider.ts
+++ src/modules/database/database.provider.ts
@@ -2,6 +2,12 @@ import { ConfigService } from '@nestjs/config';
 import { Sequelize } from 'sequelize-typescript';
 
 import { PROVIDERS, CONFIG } from '../../common/constants';
+import { Activity } from '../activities/entities/activity.model';
+import { Certification } from '../certifications/entities/certification.model';
+import { Education } from '../educations/entities/education.model';
+import { Experiences } from '../experiences/entities/experiences.model';
+import { Profiles } from '../profile/entities/profile.model';
+import { ProfilesSkills, Skills } from '../skills/entities/skill.model';
 
 import { Users } from '../user/user.model';
 
@@ -12,7 +18,16 @@ export const databaseProviders = [
       const sequelize = new Sequelize({
         ...configService.get(CONFIG.DATABASE),
       });
-      sequelize.addModels([Users]);
+      sequelize.addModels([
+        Users,
+        Profiles,
+        ProfilesSkills,
+        Skills,
+        Experiences,
+        Education,
+        Certification,
+        Activity,
+      ]);
       return sequelize;
     },
     inject: [ConfigService],
